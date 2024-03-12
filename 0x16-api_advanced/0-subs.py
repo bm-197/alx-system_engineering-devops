d38be10fe57e34d3967729d0bf7a875093c6449d
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2024-03-12 16:09:26 +0300

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,7 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
 
 def number_of_subscribers(subreddit):
     """"Get Subreddit subs number"""
-    req = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json",
-                       headers=headers, allow_redirects=False)
+    req = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json", headers=headers, allow_redirects=False)
     if (req.json().get("kind") == 't5'):
         data = req.json().get("data").get("subscribers")
         return data
