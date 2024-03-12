92eb20022d38c5391b72a25c54ae6974b4a897b5
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2024-03-12 16:08:30 +0300

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,15 @@
-#!/usr/bin/python3
-'''
-module for function to query the Reddit API
-'''
-
-"""Function to query subscribers on a given Reddit subreddit."""
 import requests
+from sys import argv
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "Mozilla/10.0/API"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    """"Get Subreddit subs number"""
+    req = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json",
+                       headers=headers, allow_redirects=False)
+    if (req.json().get("kind") == 't5'):
+        data = req.json().get("data").get("subscribers")
+        return data
+    else:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
